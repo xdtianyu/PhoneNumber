626325fb08ef6d216ac2d3730893c69a819a1709
tianyu <xdtianyu@gmail.com>
2016-05-25 21:50:06 +0800

    update common db

--- phone-number/src/main/java/org/xdty/phone/number/model/common/CommonHandler.java
+++ phone-number/src/main/java/org/xdty/phone/number/model/common/CommonHandler.java
@@ -1,8 +1,10 @@
 package org.xdty.phone.number.model.common;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.preference.PreferenceManager;
 
 import org.xdty.phone.number.R;
 import org.xdty.phone.number.model.INumber;
@@ -13,10 +15,23 @@ import java.io.File;
 
 public class CommonHandler implements NumberHandler<CommonNumber> {
 
+    public final static String COMMON_VERSION_CODE_KEY = "common_db_version_code_key";
+    public final static int COMMON_VERSION_CODE = 1;
+    public final static String DB_NAME = "common.db";
+
     private Context mContext;
 
     public CommonHandler(Context context) {
-        mContext = context;
+        mContext = context.getApplicationContext();
+        checkVersion();
+    }
+
+    private void checkVersion() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
+        if (prefs.getInt(COMMON_VERSION_CODE_KEY, 0) < COMMON_VERSION_CODE) {
+            Utils.removeCacheFile(mContext, DB_NAME);
+        }
+        prefs.edit().putInt(COMMON_VERSION_CODE_KEY, COMMON_VERSION_CODE).apply();
     }
 
     @Override
@@ -41,12 +56,12 @@ public class CommonHandler implements NumberHandler<CommonNumber> {
         Cursor cur = null;
         SQLiteDatabase db = null;
         try {
-            File dbFile = Utils.createCacheFile(mContext, "common.db", R.raw.common);
+            File dbFile = Utils.createCacheFile(mContext, DB_NAME, R.raw.common);
 
             db = SQLiteDatabase.openOrCreateDatabase(dbFile, null);
 
             cur = db.rawQuery("SELECT * FROM phone_number WHERE number = ? OR number = ? ",
-                    new String[]{number, "0" + number});
+                    new String[] { number, "0" + number });
 
             if (cur.getCount() == 1 && cur.moveToFirst()) {
                 String name = cur.getString(cur.getColumnIndex("name"));
--- phone-number/src/main/java/org/xdty/phone/number/model/marked/MarkedHandler.java
+++ phone-number/src/main/java/org/xdty/phone/number/model/marked/MarkedHandler.java
@@ -1,8 +1,10 @@
 package org.xdty.phone.number.model.marked;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.preference.PreferenceManager;
 
 import org.xdty.phone.number.R;
 import org.xdty.phone.number.model.INumber;
@@ -13,10 +15,23 @@ import java.io.File;
 
 public class MarkedHandler implements NumberHandler<MarkedNumber> {
 
+    public final static String MARKED_VERSION_CODE_KEY = "marked_db_version_code_key";
+    public final static int MARKED_VERSION_CODE = 1;
+    public final static String DB_NAME = "marked.db";
+
     private Context mContext;
 
     public MarkedHandler(Context context) {
-        mContext = context;
+        mContext = context.getApplicationContext();
+        checkVersion();
+    }
+
+    private void checkVersion() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
+        if (prefs.getInt(MARKED_VERSION_CODE_KEY, 0) < MARKED_VERSION_CODE) {
+            Utils.removeCacheFile(mContext, DB_NAME);
+        }
+        prefs.edit().putInt(MARKED_VERSION_CODE_KEY, MARKED_VERSION_CODE).apply();
     }
 
     @Override
@@ -40,12 +55,12 @@ public class MarkedHandler implements NumberHandler<MarkedNumber> {
         SQLiteDatabase db = null;
         Cursor cur = null;
         try {
-            File dbFile = Utils.createCacheFile(mContext, "marked.db", R.raw.marked);
+            File dbFile = Utils.createCacheFile(mContext, DB_NAME, R.raw.marked);
 
             db = SQLiteDatabase.openOrCreateDatabase(dbFile, null);
 
             cur = db.rawQuery("SELECT * FROM phone_number WHERE number = ? OR number = ? ",
-                    new String[]{number, "0" + number});
+                    new String[] { number, "0" + number });
 
             if (cur.getCount() == 1 && cur.moveToFirst()) {
                 int type = cur.getInt(cur.getColumnIndex("type"));
